03d00b5318cd6763ad79d12835bf6e82fc7e6c8d
Allan Cheow <allan.cheow132@gmail.com>
2021-05-05 13:42:49 -0700

    added routes and listening port

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const { projects } = require('./data.json');
-const path = require('path');
 const app = express();
+const path = require('path');
 
-// initiate pug template for express
+// initiated an external route file
+var router = require('./routes/index');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// linking folder for static assets
+app.use('/static', express.static('public'));
+
+app.use('/', router);
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000!');
+});

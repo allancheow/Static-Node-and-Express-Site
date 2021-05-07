3ef23218cc84d2f9c467da8c0aa4eede9bc62337
Allan Cheow <allan.cheow132@gmail.com>
2021-05-06 17:10:08 -0700

    added content

--- app.js
+++ app.js
@@ -12,7 +12,22 @@ app.set('view engine', 'pug');
 // linking folder for static assets
 app.use('/static', express.static('public'));
 
+// routes to index.js file in router folder
 app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000!');

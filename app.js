fc7ac3fe3528db5e3c2b67547eed7c1318b15893
Allan Cheow <allan.cheow132@gmail.com>
2021-05-10 13:30:02 -0700

    updated error handler

--- app.js
+++ app.js
@@ -17,17 +17,15 @@ app.use('/static', express.static('public'));
 app.use('/', router);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+  err.status === 404
+    ? res.status(404).render('page-not-found', { err })
+    : res.status(err.status || 500).render('error', { err });
 });
 
 app.listen(3000, () => {

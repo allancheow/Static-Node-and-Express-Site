97fab5c3d739e4c71dee22b107768861f1ed3f6d
Allan Cheow <allan.cheow132@gmail.com>
2021-05-11 14:21:09 -0700

    updated listening port for heroku deployment

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.use((err, req, res, next) => {
     : res.status(err.status || 500).render('error', { err });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is listening on port 3000!');
 });
